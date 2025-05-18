7e84a5c2e0354f18828774e769f31a72061ef3b0
ARMURIII <clement13122@gmail.com>
2025-05-18 11:24:33 +0200

    Optimized EffectIconBackgroundInventoryMixin.java and EffectIconBackgroundMixin.java using MixinExtra for Mod Compatibility

--- src/main/java/net/yirmiri/dungeonsdelight/core/mixin/EffectIconBackgroundInventoryMixin.java
+++ src/main/java/net/yirmiri/dungeonsdelight/core/mixin/EffectIconBackgroundInventoryMixin.java
@@ -1,6 +1,6 @@
 package net.yirmiri.dungeonsdelight.core.mixin;
 
-import net.minecraft.client.gui.GuiGraphics;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -10,8 +10,9 @@ import net.yirmiri.dungeonsdelight.common.util.DDUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 //The code in this file is owned by Hecco.
 //All rights are reserved by the original author.
@@ -25,7 +26,30 @@ public class EffectIconBackgroundInventoryMixin {
     //@Unique
     //private static final ResourceLocation INVENTORY_LOCATION = new ResourceLocation("textures/gui/container/inventory.png");
 
-    @Inject(method = "renderBackgrounds", at = @At("TAIL"))
+    @ModifyArg(method = "renderBackgrounds",at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIIIII)V"),index = 0)
+    private ResourceLocation dungeonsdelight$changeBackgroundTexture(ResourceLocation original, @Local MobEffectInstance mobEffectInstance) {
+        if (DDConfigClient.MONSTER_EFFECT_BACKGROUND.get() && DDUtil.MONSTER_EFFECTS.contains(mobEffectInstance.getEffect())) {
+            return MONSTER_EFFECT_BACKGROUND_TEXTURE;
+        }
+        return original;
+    }
+    @ModifyArg(method = "renderBackgrounds",at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIIIII)V"),index = 3)
+    private int dungeonsdelight$changeBackgroundWideTexture(int original, @Local(argsOnly = true) boolean b, @Local MobEffectInstance mobEffectInstance) {
+        if (DDConfigClient.MONSTER_EFFECT_BACKGROUND.get() && DDUtil.MONSTER_EFFECTS.contains(mobEffectInstance.getEffect())) {
+            return b ? 32 : 0;
+        }
+        return original;
+    }
+    @ModifyArg(method = "renderBackgrounds",at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIIIII)V"),index = 4)
+    private int dungeonsdelight$changeBackgroundTextureV(int original, @Local(argsOnly = true) boolean b, @Local MobEffectInstance mobEffectInstance) {
+        if (DDConfigClient.MONSTER_EFFECT_BACKGROUND.get() && DDUtil.MONSTER_EFFECTS.contains(mobEffectInstance.getEffect())) {
+            return 24;
+        }
+        return original;
+    }
+
+
+    /*@Inject(method = "renderBackgrounds", at = @At("TAIL"))
     private void renderBackgrounds(GuiGraphics graphics, int i1, int i2, Iterable<MobEffectInstance> instances, boolean b, CallbackInfo ci) {
         int i = ((AbstractContainerScreenMixin) this).getY();
 
@@ -49,5 +73,5 @@ public class EffectIconBackgroundInventoryMixin {
                 i += i2;
             }
         }
-    }
+    }*/
 }
--- src/main/java/net/yirmiri/dungeonsdelight/core/mixin/EffectIconBackgroundMixin.java
+++ src/main/java/net/yirmiri/dungeonsdelight/core/mixin/EffectIconBackgroundMixin.java
@@ -1,21 +1,10 @@
 package net.yirmiri.dungeonsdelight.core.mixin;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Ordering;
-import com.mojang.blaze3d.systems.RenderSystem;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.resources.MobEffectTextureManager;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraftforge.client.extensions.common.IClientMobEffectExtensions;
 import net.yirmiri.dungeonsdelight.DDConfigClient;
 import net.yirmiri.dungeonsdelight.DungeonsDelight;
 import net.yirmiri.dungeonsdelight.common.util.DDUtil;
@@ -23,11 +12,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Collection;
-import java.util.List;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 //The code in this file is owned by Hecco.
 //All rights are reserved by the original author.
@@ -42,8 +29,29 @@ public class EffectIconBackgroundMixin {
 
     @Unique
     private static final ResourceLocation MONSTER_EFFECT_BACKGROUND_TEXTURE = new ResourceLocation(DungeonsDelight.MOD_ID, "textures/gui/sprites/effect/monster_mob_effect.png");
+    @ModifyArg(method = "renderEffects",at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIIIII)V"),index = 0)
+    protected ResourceLocation dungeonsdelight$changeBackgroundTexture(ResourceLocation original, @Local MobEffectInstance mobEffectInstance) {
+        if (DDConfigClient.MONSTER_EFFECT_BACKGROUND.get() && DDUtil.MONSTER_EFFECTS.contains(mobEffectInstance.getEffect())) {
+            return MONSTER_EFFECT_BACKGROUND_TEXTURE;
+        }
+        return original;
+    }
+    @ModifyArg(method = "renderEffects",at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIIIII)V"),index = 3)
+    protected int dungeonsdelight$changeBackgroundAmbientTexture(int original, @Local MobEffectInstance mobEffectInstance) {
+        if (DDConfigClient.MONSTER_EFFECT_BACKGROUND.get() && DDUtil.MONSTER_EFFECTS.contains(mobEffectInstance.getEffect())) {
+            return mobEffectInstance.isAmbient() ? 24 : 0;
+        }
+        return original;
+    }
+    @ModifyArg(method = "renderEffects",at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIIIII)V"),index = 4)
+    protected int dungeonsdelight$changeBackgroundV(int original, @Local MobEffectInstance mobEffectInstance) {
+        if (DDConfigClient.MONSTER_EFFECT_BACKGROUND.get() && DDUtil.MONSTER_EFFECTS.contains(mobEffectInstance.getEffect())) {
+            return 0;
+        }
+        return original;
+    }
 
-    @Inject(method = "renderEffects", at = @At("TAIL"), cancellable = true)
+    /*@Inject(method = "renderEffects", at = @At("TAIL"), cancellable = true)
     public void dungeonsdelight$renderMonsterEffects(GuiGraphics graphics, CallbackInfo ci) {
         if (DDConfigClient.MONSTER_EFFECT_BACKGROUND.get()) {
             Collection<MobEffectInstance> collection = this.minecraft.player.getActiveEffects();
@@ -127,5 +135,5 @@ public class EffectIconBackgroundMixin {
                 list.forEach(Runnable::run);
             }
         }
-    }
+    }*/
 }
