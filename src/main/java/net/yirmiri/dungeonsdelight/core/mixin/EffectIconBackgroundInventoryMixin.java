ab012718d0c12390d6039f02923c26d82011bace
ARMURIII <clement13122@gmail.com>
2025-05-18 11:44:28 +0200

    Optimized EffectIconBackgroundInventoryMixin.java and EffectIconBackgroundMixin.java using MixinExtra for Mod Compatibility (removed AbstractContainerScreenMixin.java since it isn't used anymore)

--- src/main/java/net/yirmiri/dungeonsdelight/core/mixin/EffectIconBackgroundInventoryMixin.java
+++ src/main/java/net/yirmiri/dungeonsdelight/core/mixin/EffectIconBackgroundInventoryMixin.java
@@ -12,6 +12,9 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
+// made by ARMURIII
+// All Rights Reserved
+
 @Mixin(EffectRenderingInventoryScreen.class)
 public class EffectIconBackgroundInventoryMixin {
 
--- src/main/java/net/yirmiri/dungeonsdelight/core/mixin/EffectIconBackgroundMixin.java
+++ src/main/java/net/yirmiri/dungeonsdelight/core/mixin/EffectIconBackgroundMixin.java
@@ -14,6 +14,9 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
+// made by ARMURIII
+// All Rights Reserved
+
 @Mixin(Gui.class)
 public class EffectIconBackgroundMixin {
 
