b965e6a8a52a9e1e069f88c299d361d469a20935
Yirmiri <yirmirilucy@gmail.com>
2026-05-17 23:41:07 -0400

    and a bunch of broken glass...

--- common/src/main/java/net/yirmiri/dungeonsdelight/common/block/MonsterPotBlock.java
+++ common/src/main/java/net/yirmiri/dungeonsdelight/common/block/MonsterPotBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -22,5 +23,10 @@ public class MonsterPotBlock extends HorizontalDirectionalBlock {
         return Stream.of(Block.box(2, 0, 2, 14, 6, 14), Block.box(3, 6, 3, 13, 7, 13),
                 Block.box(2, 7, 2, 14, 8, 14), Block.box(13, 4, 5, 15, 6, 11),
                 Block.box(1, 4, 5, 3, 6, 11)).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get();
+    }
+
+    @Override
+    public void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 }
