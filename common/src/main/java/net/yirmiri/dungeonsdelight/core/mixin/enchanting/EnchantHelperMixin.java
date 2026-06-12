68dc02fb1ff8b8beaff718b77888021a38cbd365
Yirmiri <yirmirilucy@gmail.com>
2026-06-12 00:41:47 -0400

    venti stagionali che porteranno via questo ricordo

--- common/src/main/java/net/yirmiri/dungeonsdelight/core/mixin/enchanting/EnchantHelperMixin.java
+++ common/src/main/java/net/yirmiri/dungeonsdelight/core/mixin/enchanting/EnchantHelperMixin.java
@@ -12,9 +12,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(EnchantmentHelper.class)
-public class EnchantHelperMixin {
-    @WrapOperation(method = "getAvailableEnchantmentResults", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentCategory;canEnchant(Lnet/minecraft/world/item/Item;)Z"))
-    private static boolean dundelight$inwithstandEnch(EnchantmentCategory instance, Item item, Operation<Boolean> original, @Local Enchantment enchantment) {
-        return original.call(instance, item) || DDEnchantCategory.customEnchantPasser(enchantment, item);
-    }
+public class EnchantHelperMixin { //crash with forge
+//    @WrapOperation(method = "getAvailableEnchantmentResults", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentCategory;canEnchant(Lnet/minecraft/world/item/Item;)Z"))
+//    private static boolean dundelight$inwithstandEnch(EnchantmentCategory instance, Item item, Operation<Boolean> original, @Local Enchantment enchantment) {
+//        return original.call(instance, item) || DDEnchantCategory.customEnchantPasser(enchantment, item);
+//    }
 }
